00a7c7f6192f1ce5d1a61135fa5719afc260c183
Govardhan K G <137303089+Govardh1@users.noreply.github.com>
2023-07-13 19:33:55 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,14 @@
+public class Second {
+    public static void main(String[] args) {
+        pattren(4);
+
+    }
+    static void pattren(int n)
+    {
+       for( int row=0;row<=n;row++)
+       {
+        System.out.println("*****");
+       } 
+    //    System.out.println();
+    }
+}
